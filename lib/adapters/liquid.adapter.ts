eb7fb6354b1d48cead4473986c4106f693ed147e
jhelektrohub <jh@elektrohub.de>
2025-10-08 11:43:14 +0200

    lint: liquid adapter

--- lib/adapters/liquid.adapter.ts
+++ lib/adapters/liquid.adapter.ts
@@ -8,32 +8,38 @@ import { TemplateAdapter } from '../interfaces/template-adapter.interface';
 import { Liquid } from 'liquidjs';
 
 export class LiquidAdapter implements TemplateAdapter {
-	private config: Partial<Liquid['options']>;
+  private config: Partial<Liquid['options']>;
 
-	constructor(config?: Partial<Liquid['options']>) {
-		Object.assign(this.config, config);
-	}
+  constructor(config?: Partial<Liquid['options']>) {
+    Object.assign(this.config, config);
+  }
 
-	public compile(mail: any, callback: any, mailerOptions: MailerOptions): void {
-		const { context, template } = mail.data;
+  public compile(mail: any, callback: any, mailerOptions: MailerOptions): void {
+    const { context, template } = mail.data;
 
-		const templateExt = path.extname(template) || '.liquid';
-		const templateName = path.basename(template, path.extname(template));
-		const templateDir = path.isAbsolute(template)
-			? path.dirname(template)
-			: path.join(get(mailerOptions, 'template.dir', ''), path.dirname(template));
+    const templateExt = path.extname(template) || '.liquid';
+    const templateName = path.basename(template, path.extname(template));
+    const templateDir = path.isAbsolute(template)
+      ? path.dirname(template)
+      : path.join(
+          get(mailerOptions, 'template.dir', ''),
+          path.dirname(template),
+        );
 
-		const engine = new Liquid({
-			extname: templateExt,
-			root: templateDir,
-			...this.config.globals
-		});
+    const engine = new Liquid({
+      extname: templateExt,
+      root: templateDir,
+      ...this.config.globals,
+    });
 
-		engine.renderFile(templateName, context).then((body) => {
-			mail.data.html = body;
-			return callback();
-		}).catch((err) => {
-			return callback(err);
-		});
-	}
+    engine
+      .renderFile(templateName, context)
+      .then((body) => {
+        mail.data.html = body;
+        return callback();
+      })
+      .catch((err) => {
+        return callback(err);
+      });
+  }
 }
